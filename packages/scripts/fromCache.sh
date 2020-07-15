d09d40b507bf19675f364b641660dd5c1021ad77
Jeroen Peeters <contact@jeroenpeeters.me>
2020-07-15 18:14:10 +0200

    chore: move testing deps to one single deps.ts file

--- packages/scripts/fromCache.sh
+++ packages/scripts/fromCache.sh
@@ -1,1 +1,3 @@
 deno cache --reload --lock=lock.json deps.ts
+deno cache --reload --lock=packages/micro/lock.json packages/micro/deps.ts
+deno cache --reload --lock=packages/testing/lock.json packages/testing/deps.ts
