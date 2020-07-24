4461bb570ded3d09ed06bedac9cd951a91112ebb
Jeroen Peeters <contact@jeroenpeeters.me>
2020-07-24 17:43:50 +0200

    Update Deno version to v1.2.1

--- packages/testing/deps.ts
+++ packages/testing/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.61.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.61.0/io/bufio.ts";
+export * from "https://deno.land/std@0.62.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.62.0/io/bufio.ts";
