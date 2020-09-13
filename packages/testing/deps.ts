61d53f44d453e70003a40bbfa92f996f031ef26c
jeroenptrs <jeroenptrs@users.noreply.github.com>
2020-09-13 16:11:23 +0000

    Update Deno version to v1.4.0

--- packages/testing/deps.ts
+++ packages/testing/deps.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/std@0.62.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.62.0/io/bufio.ts";
-export { ServerRequest } from "https://deno.land/std@0.62.0/http/server.ts";
+export * from "https://deno.land/std@0.69.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.69.0/io/bufio.ts";
+export { ServerRequest } from "https://deno.land/std@0.69.0/http/server.ts";
